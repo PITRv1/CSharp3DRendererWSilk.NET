38f082e1cfa72ce310ff3c756cc5f48630dac847
PITRv1 <petersandor.toth@gmail.com>
2026-03-01 18:12:12 +0100

    Add project files.

---
+++ ModelInstance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take4_at_rendering
+{
+    public class ModelInstance {
+        public Model Model { get; set; }
+        public Transform Transform {get; set;}
+        public Texture Texture { get; set; }
+
+        public ModelInstance(Model model, Transform transform, Texture texture) {
+            Model = model;
+            Transform = transform;
+            Texture = texture;
+        }
+    }
+}
